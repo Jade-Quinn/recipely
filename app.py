89682d8e438fd194ae6445cf4ca9ff0d4b1c2cb9
Jade Quinn <jadekquinn@gmail.com>
2022-02-19 20:07:24 +0000

    (feat): Allow user to submit task to db

--- app.py
+++ app.py
@@ -94,10 +94,20 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    if request.method == "POST":
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("get_recipes"))
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
+
 
 
 if __name__ == "__main__":
