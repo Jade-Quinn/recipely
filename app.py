41258f402a0318cc9994cfe9affa06cf8e65fc7a
Jade Quinn <jadekquinn@gmail.com>
2022-02-19 15:20:31 +0000

    (fix): Correct spelling in app.py to connect to MongoDB

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_recipies")
-def get_recipies():
-    recipies = mongo.db.recipies.find()
-    return render_template("recipies.html", recipies=recipies)
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == "__main__":
